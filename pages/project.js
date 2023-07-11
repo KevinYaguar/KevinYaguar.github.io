f8e8a43564b1d144d243f7dcb31334c0d43eee16
kevin <k.yaguar@wisboo.com>
2023-07-11 18:42:51 -0300

    validations

--- pages/project.js
+++ pages/project.js
@@ -7,14 +7,14 @@ const Project = () => {
     const router = useRouter();
     
     const currentProject = projects.filter((project) => {
-        return project.name.replaceAll(' ', '_') === router?.query?.title?.replaceAll(' ', '_')
+        return project?.name.replaceAll(' ', '_') === router?.query?.title?.replaceAll(' ', '_')
     })[0]
 
     return (
-        <Layout footer={false} title={currentProject.name}>
+        <Layout footer={false} title={currentProject?.name}>
             <div className="text-center d-flex justify-content-around">
-                <img src={`/${currentProject.image}`} alt="" className="img-fluid" style={{width: '30%'}}/>
-                <p>{currentProject.content}</p>
+                <img src={`/${currentProject?.image}`} alt="" className="img-fluid" style={{width: '30%'}}/>
+                <p>{currentProject?.content}</p>
             </div>
         </Layout>
     )
